41d1bdec7584d80a806929b2b0af7faf102075bb
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-10-03 19:03:30 +0100

    brunch

---
+++ app/initialize.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // do your setup here
+  console.log('Initialized app');
+});
+
+var $ = require('jquery');
+console.log('Tasty Brunch, just trying to use jQuery!', $('body'));
